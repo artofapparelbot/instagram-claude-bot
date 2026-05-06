53f8540bdcc552a2f0e8c4790805d01c0b0cfd64
artofapparelbot <artofapparelstudio@gmail.com>
2026-05-06 13:49:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def home():
     return "Art of Apparel Bot is running!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
